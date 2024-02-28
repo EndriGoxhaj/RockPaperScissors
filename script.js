c22090a5bb8dba06ee3b566c5fb7682952a326ea
GEndr1 <goxhajendri5@gmail.com>
2024-02-28 14:08:32 +0100

    Created a function that plays a single round

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
-function getComputerChoice(ComputerChoice) {
-    ComputerChoice =  Math.floor(Math.random() * 3);
-    if (ComputerChoice == 0){
-        return pick = "Rock"; }
-        else if (ComputerChoice == 1){
-            return pick = "Paper"; }
+function getComputerChoice() {
+  let Bet =  Math.floor(Math.random() * 3);
+    if (Bet == 0){
+        return pick = "rock"; }
+        else if (Bet == 1){
+            return pick = "paper"; }
             else { 
                 return pick = "scissors";
-            }
-
+            };
   }
-  console.log(getComputerChoice());
+
+  function gameRound(){
+    const playerSelection = prompt("Enter your choice: ");
+
+    const pick = getComputerChoice();
+  if (playerSelection.toLowerCase === pick){
+    return winner = `Tie ${playerSelection} is same as ${pick}`;}
+    else if (playerSelection.toLowerCase() === "paper" && pick === "rock" || playerSelection.toLowerCase() === "rock" && pick === "scissors" || playerSelection.toLowerCase() == "scissors" && pick === "paper"){
+        return winner = `player wins, ${playerSelection} beats ${pick}`}
+    else{
+        return winner = `computer wins, ${pick} beats ${playerSelection}`}
+
+}
+console.log(gameRound());
