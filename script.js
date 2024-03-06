504a5b5792aabe09b7f1ee95617166f194dfcf9d
GEndr1 <goxhajendri5@gmail.com>
2024-03-06 12:53:30 +0100

    added loop and finished the project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let playerScore = 0;
+let computerScore = 0;
 function getComputerChoice() {
   let Bet =  Math.floor(Math.random() * 3);
     if (Bet == 0){
@@ -10,16 +11,47 @@ function getComputerChoice() {
             };
   }
 
+
   function gameRound(){
     const playerSelection = prompt("Enter your choice: ");
-
+    console.log(playerSelection);
     const pick = getComputerChoice();
-  if (playerSelection.toLowerCase === pick){
-    return winner = `Tie ${playerSelection} is same as ${pick}`;}
-    else if (playerSelection.toLowerCase() === "paper" && pick === "rock" || playerSelection.toLowerCase() === "rock" && pick === "scissors" || playerSelection.toLowerCase() == "scissors" && pick === "paper"){
-        return winner = `player wins, ${playerSelection} beats ${pick}`}
-    else{
-        return winner = `computer wins, ${pick} beats ${playerSelection}`}
+    console.log(pick);
+  if (playerSelection.toLowerCase() === pick){
+
+    return winner = `Tie ${playerSelection} is same as ${pick}
+player score is ${playerScore} to computer score ${computerScore}`
 
 }
-console.log(gameRound());
+    else if (playerSelection.toLowerCase() === "paper" && pick === "rock" 
+    || playerSelection.toLowerCase() === "rock" && pick === "scissors" 
+    || playerSelection.toLowerCase() === "scissors" && pick === "paper"){
+
+      playerScore += 1;
+        return winner = `player wins, ${playerSelection} beats ${pick}
+player score is ${playerScore} to computer score ${computerScore}`
+ }
+    else {
+      computerScore += 1;
+        return winner = `computer wins, ${pick} beats ${playerSelection}
+player score is ${playerScore} to computer score ${computerScore}`}
+    }
+
+
+function playGame(playerSelection, pick){
+  for(let i = 0; i < 5; i++){
+  gameRound();
+  console.log(winner);
+  }
+  if(playerScore > computerScore){
+  return gameWinner = `Congratulations you won!`
+  }
+  else if ( computerScore > playerScore ){
+    return gameWinner = `Sorry you lost, reload to try again.`
+  }
+  else{
+    return gameWinner = ` it's a tie`
+  } 
+
+}
+console.log(playGame());
